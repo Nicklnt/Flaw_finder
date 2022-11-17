1f86ca4daab9c6c579263319de4931d882ce328d
Nicklnt <116169364+Nicklnt@users.noreply.github.com>
2022-11-17 10:30:09 +0100

    Update sample.c

--- sample.c
+++ sample.c
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
     char buff[15];
     int pass = 0;
     printf("something something");
+    printf("another printf")
 
     printf("\n Enter the password : \n");
     gets(buff);
